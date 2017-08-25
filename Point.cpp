784441f177f166a1e670dcca9a102754d6a875bf
Paul Slavin <pmslavin@gmail.com>
2017-08-25 01:00:14 +0100

    Radius-based collision

--- Point.cpp
+++ Point.cpp
@@ -1,4 +1,5 @@
 #include "Point.hpp"
+#include "math.h"
 
 Point::Point(double x, double y, double dx, double dy) : x(x), y(y), dx(dx), dy(dy)
 {
@@ -12,17 +13,45 @@ Point::~Point()
 
 bool Point::collides(Point &other)
 {
-	int ix = int(x);
-	int iy = int(y);
-	int ox = int(other.x);
-	int oy = int(other.y);
+	double xd = x - other.x;
+	double yd = y - other.y;
+	double hypot = sqrt(xd*xd+yd*yd);
 
-	if(((ix <= ox && ox <= ix+P_SZ) && (iy <= oy && oy <= iy+P_SZ)) || ((ox <= ix && ix <= ox+P_SZ) && (oy <= iy && iy <= oy+P_SZ))){
-		/* Fix */
-		dx = -dx;
-		dy = -dy;
-		other.dx = -other.dx;
-		other.dy = -other.dy;
+	/* Fix */
+	if(hypot < 2.0*P_SZ){
+		double cdrag = 0.8401;
+
+		double lhspeed  = sqrt(dx*dx+dy*dy);
+		double rhspeed  = sqrt(other.dx*other.dx+other.dy*other.dy);
+
+		double theta = atan2(yd,xd) + M_PI_2;
+		double phi   = atan2(dy,dx) + M_PI_2;
+		double rho   = atan2(other.dy,other.dx) + M_PI_2;
+
+		double av_x = sin(theta) * rhspeed;
+		double av_y = cos(theta) * rhspeed;
+
+		double av_angle = M_PI_2 - atan2(av_y, av_x);
+		double av_len   = sqrt(av_x*av_x + av_y*av_y);
+
+		dx = sin(av_angle)*av_len*cdrag;;
+		dy = cos(av_angle)*av_len*cdrag;;
+
+		av_x = sin(theta+M_PI) * lhspeed;
+		av_y = cos(theta+M_PI) * lhspeed;
+
+		av_angle = M_PI_2 - atan2(av_y, av_x);
+		av_len   = sqrt(av_x*av_x + av_y*av_y);
+
+		other.dx = sin(av_angle)*av_len*cdrag;;
+		other.dy = cos(av_angle)*av_len*cdrag;;
+
+		double intersect = 0.5*(2*P_SZ - hypot+1.0);
+		x += sin(theta)*intersect;
+		y -= cos(theta)*intersect;
+		other.x -= sin(theta)*intersect;
+		other.y += cos(theta)*intersect;
+
 		return true;
 	}
 
@@ -35,9 +64,11 @@ void Point::step()
 	x += dx;
 	y += dy;
 
-	if(x < 1.0 || x >= W-1)
+	dy += 0.07;
+
+	if(x-P_SZ < 1 || x+P_SZ >= W)
 		dx = -dx;
 
-	if(y < 1.0 || y >= H-1)
+	if(y-P_SZ < 1 || y+P_SZ >= H)
 		dy = -dy;
 }
--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@
 double randFunc();
 
 int fps = 30;
+int npoints = 240;
 
 void test_tree()
 {
 	initialise();
 	std::vector<Point> points;
 
-	for(int i=0; i<480; ++i)
-		points.push_back({i*W/480.0, H/2+rand()%H/2*sin(2*i*M_PI/240.0), randFunc(), randFunc()});
+	for(int i=0; i<npoints; ++i)
+		points.push_back({i*W/(double)npoints, H/2+rand()%H/2*sin(2*i*M_PI/(npoints/2.0)), randFunc(), randFunc()});
 //		points.push_back({(int)(i*W/200.0), (int)(H/2+rand()%H/2*sin(2*i*M_PI/100.0)), (double)6.0*(drand48()-0.5), (double)6.0*(drand48()-0.5)} );
 //		points.push_back({(int)(i*W/200.0), (int)(H/2+H/2*sin(i*M_PI/100.0))});
 
@@ -28,9 +29,10 @@ void test_tree()
 
 	for(auto &p: points){
 		drawPoint(p);
-		update();
-		pause(10);
+//		update();
+//		pause(10);
 	}
+	update();
 
 	wait();
 
@@ -39,9 +41,9 @@ void test_tree()
 		for(auto &p: points){
 			steptree.insert(&p);
 			drawPoint(p);
-//			drawQuadtree(&steptree);
 			p.step();
 		}
+//		drawQuadtree(&steptree);
 		update();
 		steptree.calcCollisions();
 		pause(1000.0/fps);
@@ -54,7 +56,7 @@ void test_tree()
 
 double randFunc()
 {
-	return ((rand()|1)&15)*(drand48()-0.5);
+	return ((rand()|1)&7)*(drand48()-0.5);
 }
 
 
